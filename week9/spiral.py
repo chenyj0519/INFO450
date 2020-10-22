f807a62f7ce1a55a720dd845b191c4e2d4642687
chenyj0519 <70119553+chenyj0519@users.noreply.github.com>
2020-10-22 14:56:20 -0400

    Update spiral.py

--- week9/spiral.py
+++ week9/spiral.py
@@ -1,6 +1,6 @@
 def sum(i):
     n = (i-1) // 2
-    return (16*n**3 + 30*n**2 + 26*n + 3) // 3
+    return 1+2*n*(8*n*n+15*n+13)/3
 
-i = int(input("Enter 501: "))
+i = int(input("Enter the number: "))
 print (" The sum is ",sum(i))
