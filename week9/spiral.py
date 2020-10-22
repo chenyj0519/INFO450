1370454f048128e4829ff727376ed655bf6a48e3
chenyj0519 <70119553+chenyj0519@users.noreply.github.com>
2020-10-22 13:52:14 -0400

    Update spiral.py

--- week9/spiral.py
+++ week9/spiral.py
@@ -1,9 +1,6 @@
-#verified the sume is 101
-#501 x 501
-
 def sum(i):
-    n = (i-1)/2
-    return 16*n*n*n+30*n*n+26*n+3)/3
-    
-i = int(input("Enter 501"))
-print ("the answer is", sum(i))
+    n = (i-1) // 2
+    return (16*n**3 + 30*n**2 + 26*n + 3) // 3
+
+i = int(input("Enter 501: "))
+print (" The sum is ",sum(i))
